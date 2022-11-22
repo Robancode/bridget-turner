6003f85f748d8de4b8e41280aceab032cb41283e
Rosita <rositabannert@hotmail.co.uk>
2022-11-22 12:05:08 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>Coming soon...</header>
+    </div>
+  );
+}
+
+export default App;
